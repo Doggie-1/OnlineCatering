bce7ff83ae8936146188ef6c3d10cd08caa22333
Gerald Stephen Alina <geraldstephenalina99@gmail.com>
2022-11-24 14:05:22 +0800

    Change 12 to 10.

--- noncombo.php
+++ noncombo.php
@@ -10,7 +10,7 @@ endif;
 	<?php include 'menu-tab.php';?>
 	
 		<div class = "content">
-			<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-10">
 				<div class = "col-md-9 col-lg-9">
 					<div class="widget wgreen">
                 
