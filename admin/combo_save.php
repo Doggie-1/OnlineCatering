70283055eb58e8cef83f1a24d1eb40bfd4de4603
Marcelino Yu <yanmingrui27@gmail.com>
2022-12-08 22:51:57 +0800

    Fixed handling empty array on saving/updating combo_details.

--- admin/combo_save.php
+++ admin/combo_save.php
@@ -4,18 +4,20 @@ include('../includes/dbcon.php');
 	
 	$name = $_POST['name'];
 	$price = $_POST['price'];
-	$menu = $_POST['menu'];
+	$menu = array_key_exists("menu",$_POST) ? $_POST['menu'] : [];
 	
 	mysqli_query($con,"INSERT INTO combo(combo_name,combo_price) 
 			VALUES('$name','$price')")or die(mysqli_error());  
 	$id=mysqli_insert_id($con);
-	foreach ($menu as $value)
-	{
-		if ($value<>0)
-		{
-		mysqli_query($con,"INSERT INTO combo_details(combo_id,menu_id) 
-			VALUES('$id','$value')")or die(mysqli_error());  
-		}
+	if (!empty($menu)) {
+        foreach ($menu as $value)
+        {
+            if ($value<>0)
+            {
+            mysqli_query($con,"INSERT INTO combo_details(combo_id,menu_id)
+                VALUES('$id','$value')")or die(mysqli_error());
+            }
+        }
 	}
 			echo "<script type='text/javascript'>alert('Successfully added new combo meal!');</script>";
 			echo "<script>document.location='combo.php'</script>";   
