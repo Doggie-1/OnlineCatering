3e0cf9ead17e975b426a84c37fb4435b7be2f4d6
Marcelino Yu <yanmingrui27@gmail.com>
2022-12-08 23:08:37 +0800

    Fixed layout arrangement.

--- reservation_status.php
+++ reservation_status.php
@@ -4,7 +4,16 @@ header('Location:index.php');
 endif;
 ?>
 <?php include 'header.php';?>
-
+<style>
+    .grid-container {
+        display: grid;
+        grid-template-columns: auto auto;
+        padding: 10px;
+    }
+    .grid-item {
+        padding: 20px;
+    }
+</style>
 <body>
   <?php include 'navbar.php';?>
   <?php include 'menu-tab.php';?>
@@ -36,113 +45,110 @@ endif;
 <h4 style="text-align:center">Facebook : facebook.com/progaddictbylee</h4>
 <hr>
 
-<table style="width:100%">
-<?php
-include('includes/dbcon.php');
-    $rcode=$_REQUEST['rcode'];
-    $query=mysqli_query($con,"select * from reservation where r_code='$rcode'")or die(mysqli_error($con));
-    $row=mysqli_fetch_array($query);
-    $rcode=$row['r_code'];
-    $first=$row['r_first'];
-    $last=$row['r_last'];
-    $contact=$row['r_contact'];
-    $address=$row['r_address'];
-    $date=$row['r_date'];
-    $venue=$row['r_venue'];
-    $balance=$row['balance'];
-    $payable=$row['payable'];
-    $ocassion=$row['r_ocassion'];
-    $team=$row['team_id'];
-    $status=$row['r_status'];
-    $motif=$row['r_motif'];
-    $time=$row['r_time'];
-    $time=$row['r_time'];
-    $type=$row['r_type'];
-    $cid=$row['combo_id'];
-    if ($cid) {
-        $query1 = mysqli_query($con,"select * from combo where combo_id='$cid'")or die(mysqli_error($con));
-        $row1 = mysqli_fetch_array($query1);
-    }
+    <div class="grid-container">
+        <div class="grid-item">
+            <table style="width:100%">
+                <?php
+                    include('includes/dbcon.php');
+                    $rcode=$_REQUEST['rcode'];
+                    $query=mysqli_query($con,"select * from reservation where r_code='$rcode'")or die(mysqli_error($con));
+                    $row=mysqli_fetch_array($query);
+                    $rcode=$row['r_code'];
+                    $first=$row['r_first'];
+                    $last=$row['r_last'];
+                    $contact=$row['r_contact'];
+                    $address=$row['r_address'];
+                    $date=$row['r_date'];
+                    $venue=$row['r_venue'];
+                    $balance=$row['balance'];
+                    $payable=$row['payable'];
+                    $ocassion=$row['r_ocassion'];
+                    $team=$row['team_id'];
+                    $status=$row['r_status'];
+                    $motif=$row['r_motif'];
+                    $time=$row['r_time'];
+                    $time=$row['r_time'];
+                    $type=$row['r_type'];
+                    $cid=$row['combo_id'];
+                    if ($cid) {
+                        $query1 = mysqli_query($con,"select * from combo where combo_id='$cid'")or die(mysqli_error($con));
+                        $row1 = mysqli_fetch_array($query1);
+                    }
 
-    $cname = !empty($row1) ? $row1['combo_name'] : "Custom Package";
-?>                      
-                      <tr>
-                        <td>RCode: </td>
-                        <td><?php echo $rcode;?></td>
-                        <td>Event Date: </td>
-                        <td><?php echo date("M d, Y",strtotime($date));?></td>
-                      </tr>
-                      <tr>  
-                        <td>Name: </td>
-                        <td><?php echo $last." ,".$first;?></td>
-                        <td>Time: </td>
-                        <td><?php echo date("h:i a",strtotime($time));?></td>
-                      </tr>
-                      <tr>
-                        <td>Contact #: </td>
-                        <td><?php echo $contact;?></td>
-                        <td>Venue: </td>
-                        <td><?php echo $venue;?></td>
-                      </tr> 
-                      <tr>
-                        <td>Address: </td>
-                        <td><?php echo $address;?></td>
-                        <td>Motif: </td>
-                        <td><?php echo $motif;?></td>
-
-                      </tr>   
-                      <tr>  
-                        <td>Status:</td>
-                        <th><?php echo $row['r_status'];?></th>
-                        <td>Occasion: </td>
-                        <td><?php echo $ocassion;?></td>
-                        
-                      </tr>  
-                      <tr>  
-                        <td></td>
-                        <td></td>
-                        <td>Type: </td>
-                        <td><?php echo $type;?></td>
-                        
-                      </tr>  
-                      <tr>  
-                        <td></td>
-                        <td></td>
-                        <td>No. of Pax: </td>
-                        <td><?php echo $row['pax'];?></td>
-                        
-                      </tr>  
-
-                      <tr>  
-                        <td></td>
-                        <td></td>
-                        <td>Payable: </td>
-                        <td>P <?php echo number_format($payable,2);?></td>
-                      </tr> 
-                      <tr>  
-                        <td></td>
-                        <td></td>
-                        <td>Mode of Payment: </td>
-                        <td><?php echo $row['modeofpayment'];?></td>
-                      </tr> 
-                      
-</table>
-<br>
-<div style="width:50%;float:left">
-  <h4><?php echo $cname;?></h4>
-  <span>No. of persons: <?php echo $row['pax'];?> * <?php echo $row['price'];?> = <?php echo $row['payable'];?></span>
-  <ul>
-
-<?php
-    
-    $query1=mysqli_query($con,"select * from combo_details natural join menu where combo_id='$cid'")or die(mysqli_error($con));
-      while($row1=mysqli_fetch_array($query1))
-      {
-  ?>    
-    <li><?php echo  $row1['menu_name'];?></li>
-
-<?php }?>    
-</div>
+                    $cname = !empty($row1) ? $row1['combo_name'] : "Custom Package";
+                ?>
+                <tr>
+                    <td>RCode: </td>
+                    <td><?php echo $rcode;?></td>
+                </tr>
+                <tr>
+                    <td>Name: </td>
+                    <td><?php echo $last." ,".$first;?></td>
+                </tr>
+                <tr>
+                    <td>Contact #: </td>
+                    <td><?php echo $contact;?></td>
+                </tr>
+                <tr>
+                    <td>Address: </td>
+                    <td><?php echo $address;?></td>
+                </tr>
+                <tr>
+                    <td>Status:</td>
+                    <th><?php echo $row['r_status'];?></th>
+                </tr>
+                <tr>
+                    <td>Event Date: </td>
+                    <td><?php echo date("M d, Y",strtotime($date));?></td>
+                </tr>
+                <tr>
+                    <td>Time: </td>
+                    <td><?php echo date("h:i a",strtotime($time));?></td>
+                </tr>
+                <tr>
+                    <td>Venue: </td>
+                    <td><?php echo $venue;?></td>
+                </tr>
+                <tr>
+                    <td>Motif: </td>
+                    <td><?php echo $motif;?></td>
+                </tr>
+                <tr>
+                    <td>Occasion: </td>
+                    <td><?php echo $ocassion;?></td>
+                </tr>
+                <tr>
+                    <td>Type: </td>
+                    <td><?php echo $type;?></td>
+                </tr>
+                <tr>
+                    <td>No. of Pax: </td>
+                    <td><?php echo $row['pax'];?></td>
+                </tr>
+                <tr>
+                    <td>Payable: </td>
+                    <td>P <?php echo number_format($payable,2);?></td>
+                </tr>
+                <tr>
+                    <td>Mode of Payment: </td>
+                    <td><?php echo $row['modeofpayment'];?></td>
+                </tr>
+            </table>
+        </div>
+        <div class="grid-item">
+            <div style="width:50%;float:left">
+                <h4><?php echo $cname;?></h4>
+                <span>No. of persons: <?php echo $row['pax'];?> * <?php echo $row['price'];?> = <?php echo $row['payable'];?></span>
+                <?php
+                    $query1=mysqli_query($con,"select * from combo_details natural join menu where combo_id='$cid'")or die(mysqli_error($con));
+                    while($row1=mysqli_fetch_array($query1))
+                    {
+                ?>
+                    <li><?php echo  $row1['menu_name'];?></li>
+                <?php }?>
+            </div>
+        </div>
+    </div>
 
 
                   </div><!--pad-->  
