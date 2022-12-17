77ee3787d81218bc095d6982255e59e804eb9344
Doggie-1 <118423031+Doggie-1@users.noreply.github.com>
2022-12-17 14:51:47 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-12">
             <!-- Copyright info -->
-            <p class="copy">Copyright &copy; 2021 | <a href="#">CampCodes Catering</a> </p>
+            <p class="copy">Copyright &copy; 2022 | <a href="https://www.facebook.com/Magic.Kan.anan">Kirby's Catering Services</a> </p>
       </div>
     </div>
   </div>
