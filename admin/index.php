4ef1bea3e47f3d0b267d6baa85c229d9a37f3328
Doggie-1 <118423031+Doggie-1@users.noreply.github.com>
2022-12-27 09:26:36 +0800

    Changing the default name into catering's name

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,7 @@
 <div class="lockscreen-wrapper">
   <div class="lockscreen-logo">
     
-    <a style = "color:white;" href="../index.php">Catering</a>
+    <a style = "color:white;" href="../index.php">Kirby's Magic Kan-anan Catering Services</a>
   </div>
   <!-- User name -->
   <div class="lockscreen-name">Administrator</div>
@@ -22,8 +22,8 @@
     <!-- lockscreen credentials (contains the form) -->
     <form class="lockscreen-credentials" action = "login.php"method = "POST">
       <div class="input-group">
-        <input type="username" name = "username" class="form-control" placeholder="username" >
-        <input type="password" name = "password" class="form-control" placeholder="password" autofocus>
+        <input type="username" name = "username" class="form-control" placeholder="username" autofocus>
+        <input type="password" name = "password" class="form-control" placeholder="password" >
 
         <div class="input-group-btn">
           <button name = "login"class="btn"><i class="fa fa-arrow-right "></i></button>
