ffbf17d6cd3f3b742886c2ff55276f84c1a06b31
Doggie-1 <118423031+Doggie-1@users.noreply.github.com>
2022-12-28 11:47:43 +0800

    Adjusted the font

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -5,21 +5,21 @@
         <!-- If the main navigation has sub navigation, then add the class "has_sub" to "li" of main navigation. -->
         <ul id="nav">
           <!-- Main menu with font awesome icon -->
-          <li class="open"><a href="dashboard.php"><i class="fa fa-home"></i> Dashboard</a>
+		<li class="open"><a href="dashboard.php"><i class="fa fa-home"></i> <b>Dashboard</b></a>
           </li>
           <li class="has_sub">
-			<a href="#"><i class="fa fa-calendar"></i> Reservation  <span class="pull-right"><i class="fa fa-chevron-right"></i></span></a>
+		  <a href="#"><i class="fa fa-calendar"></i> <b>Reservation Status</b> <span class="pull-right"><i class="fa fa-chevron-right"></i></span></a>
             <ul>
-		    <li><a href="reservation.php"><b>Confirmed</b></a></li>
               <li><a href="pending.php"><b>Pending</b></a></li>
+	      <li><a href="reservation.php"><b>Confirmed</b></a></li>
               <li><a href="finished.php"><b>Finished</b></a></li>
               <li><a href="cancelled.php"><b>Cancelled</b></a></li>
             </ul>
           </li>  
-          <li><a href="combo.php"><i class="fa fa-bar-chart-o"></i> Combo</a></li> 
+		<li><a href="combo.php"><i class="fa fa-bar-chart-o"></i> <b>Combo</b></a></li> 
           
           <li class="has_sub">
-			<a href="#"><i class="fa fa-file-o"></i> Maintenance  <span class="pull-right"><i class="fa fa-chevron-right"></i></span></a>
+		  <a href="#"><i class="fa fa-file-o"></i> <b>Maintenance</b>  <span class="pull-right"><i class="fa fa-chevron-right"></i></span></a>
             <ul>			
 		    <li><a href="menu.php"><b>Menu</b></a></li>
               <li><a href="category.php"><b>Category</b></a></li>
@@ -27,7 +27,7 @@
               <li><a href="user.php"><b>User</b></a></li>
             </ul>
           </li>
-          <li><a href="messages.php"><i class="fa fa-envelope"></i> Messages</a></li>     
+		<li><a href="messages.php"><i class="fa fa-envelope"></i> <b>Messages</b></a></li>     
 		  
           <!---<li><a href="backup.php"><i class="fa fa-bar-chart-o"></i> Backup/Restore</a></li>--->
         </ul>
