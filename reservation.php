6ebeff995323736710080914fb163fe6e0668356
Gerald Stephen Alina <geraldstephenalina99@gmail.com>
2022-12-30 12:11:04 +0800

    Adding height to maintain the footer

--- reservation.php
+++ reservation.php
@@ -4,8 +4,8 @@
 	<?php include 'navbar.php';?>
 	<?php include 'menu-tab.php';?>
 	
-      <div class="container"><br><br>
-		    <div class ="content">
+      <div class="container"><br><br><br><br><br><br>
+		    <div class ="content" style="height: 75vh;">
 					<div class="widget wgreen">
                 
               <div class="widget-head">
