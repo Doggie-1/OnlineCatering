6cb8eb897a4af1fb0049a797e40d32b6fe206124
Gerald Stephen Alina <geraldstephenalina99@gmail.com>
2023-01-13 17:17:24 +0800

    Changing the terms and condition

--- facilities_modal.php
+++ facilities_modal.php
@@ -7,21 +7,19 @@
         </div>
          <div class="modal-body">
                 <div class="padd">
-					<p>  1. Catering is within the area of NEGROS OCCIDENTAL only.</p>
+					<p> 1. Catering is within the area of Cagayan de Oro City only.</p>
 
-					<p>2. Additional 10% payment on reservation outside Talisay City.</p>
+					<p> 2. MINIMUM Pax For Lunch nd Dinner 50px , Merienda 100px and in the Non-Combo Combo meal is 30px.</p>
 
-					<p>3. MINIMUM Pax For Lunch nd Dinner 50px , Merienda 100px and in the Non-Combo Combo meal is 30px.</p>
+					<p> 3. Costumer must pay 50% of the actual price as an advance payment 3 days after confirmation, if the costumer failed to pay the said advance payment reservation will be cancelled.</p>
 
-					<p>4. Costumer must pay 50% of the actual price as an advance payment 3 days after confirmation, if the costumer failed to pay the said advance payment reservation will be cancelled.</p>
+					<p> 4. The management will call the costumer about the payment details.</p>
 
-					<p>5. The management will call the costumer about the payment details.</p>
+					<p> 5. If the costumer wants to cancel its confirmed reservation due to personal reason, the management will get 25% from the advance payment he/she made as charge for the damages.</p>
 
-					<p>6. If the costumer wants to cancel its confirmed reservation due to personal reason, the management will get 25% from the advance payment he/she made as charge for the damages.</p>
+					<p> 6. 50 percent should be paid before the approval of reservation, 3 days without payments will resolve to the termination of reservation.</p>
 
-					<p>7. 50 percent should be paid before the approval of reservation, 3 days without payments will resolve to the termination of reservation.</p>
-
-					<p>8. Terms of payments will be via padala center cebuana or palawan center or personal payments.
+					<p> 7. Terms of payments will be via Gcash, bank to bank, cash.
 close</p>
 				</div>
         </div>
